4124f39ace93ebb33749ff5772c827ec2a4dc646
rustielin <rustielin@berkeley.edu>
2017-10-24 20:42:56 -0700

    title change

--- reactui/src/components/Header/Header.js
+++ reactui/src/components/Header/Header.js
@@ -52,36 +52,36 @@ class Header extends Component {
   render() {
     return (
       <header className="app-header navbar">
-        <NavbarToggler className="d-lg-none" onClick={this.mobileSidebarToggle}>&#9776;</NavbarToggler>
-        <NavbarBrand href="#"></NavbarBrand>
-        <NavbarToggler className="d-md-down-none" onClick={this.sidebarToggle}>&#9776;</NavbarToggler>
-        <Nav className="d-md-down-none" navbar>
-          <NavItem className="px-3">
-            <NavLink href="#">Dashboard</NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink href="#">Users</NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink href="#">Settings</NavLink>
-          </NavItem>
-        </Nav>
-        <Nav className="ml-auto" navbar>
-          <NavItem className="d-md-down-none">
-            <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink href="#"><i className="icon-list"></i></NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink href="#"><i className="icon-location-pin"></i></NavLink>
-          </NavItem>
-          <NavItem>
-            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-              <DropdownToggle className="nav-link dropdown-toggle">
-                <span className="d-md-down-none">admin</span>
-              </DropdownToggle>
-              <DropdownMenu right className={this.state.dropdownOpen ? 'show' : ''}>
+          <NavbarToggler className="d-lg-none" onClick={this.mobileSidebarToggle}>&#9776;</NavbarToggler>
+          <NavbarBrand href="#"></NavbarBrand>
+          <NavbarToggler className="d-md-down-none" onClick={this.sidebarToggle}>&#9776;</NavbarToggler>
+          <Nav className="d-md-down-none" navbar>
+              <NavItem className="px-3">
+                  <NavLink href="#">Dashboard</NavLink>
+              </NavItem>
+              <NavItem className="px-3">
+                  <NavLink href="#">Users</NavLink>
+              </NavItem>
+              <NavItem className="px-3">
+                  <NavLink href="#">Settings</NavLink>
+              </NavItem>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+              <NavItem className="d-md-down-none">
+                  <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
+              </NavItem>
+              <NavItem className="d-md-down-none">
+                  <NavLink href="#"><i className="icon-list"></i></NavLink>
+              </NavItem>
+              <NavItem className="d-md-down-none">
+                  <NavLink href="#"><i className="icon-location-pin"></i></NavLink>
+              </NavItem>
+              <NavItem>
+                  <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                      <DropdownToggle className="nav-link dropdown-toggle">
+                          <span className="d-md-down-none">admin</span>
+                      </DropdownToggle>
+                      <DropdownMenu right className={this.state.dropdownOpen ? 'show' : ''}>
                 <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
                 <DropdownItem><i className="fa fa-bell-o"></i> Updates<Badge color="info">42</Badge></DropdownItem>
                 <DropdownItem><i className="fa fa-envelope-o"></i> Messages<Badge color="success">42</Badge></DropdownItem>
--- reactui/src/containers/Full/Full.js
+++ reactui/src/containers/Full/Full.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {Container} from 'reactstrap';
 import Header from '../../components/Header/';
-import Sidebar from '../../components/Sidebar/';
 import Dashboard from '../../views/Dashboard/';
 import Register from '../../views/Pages/Register/';
 
@@ -10,17 +9,16 @@ class Full extends Component {
   render() {
     return (
       <div className="app">
-        <Header />
-        <div className="app-body">
-          <Sidebar {...this.props}/>
-          <main className="main">
-            <Container fluid>
-              <Switch>
-                <Route path="/dashboard" name="Dashboard" component={Dashboard}/>
-                <Route exact path="/register" name="Register Page" component={Register}/>
-                <Redirect from="/" to="/dashboard"/>
-              </Switch>
-            </Container>
+          <Header />
+          <div className="app-body">
+              <main className="main">
+                  <Container fluid>
+                      <Switch>
+                          <Route path="/dashboard" name="Dashboard" component={Dashboard}/>
+                          <Route exact path="/register" name="Register Page" component={Register}/>
+                          <Redirect from="/" to="/dashboard"/>
+                      </Switch>
+                  </Container>
           </main>
         </div>
       </div>
