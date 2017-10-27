419c92447453b6c9acfc8bd0aac6cc555e86feae
rustielin <rustielin@berkeley.edu>
2017-10-27 01:14:31 -0700

    something something

--- reactui/src/EthereumSetup.js
+++ reactui/src/EthereumSetup.js
@@ -4,14 +4,14 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 // application binary interface
 var supplyABI = [];
 
-// contract address
-var supplyAddress = '';
+// contract supply
+var supplysupply = '';
 
 // get the reference
 const supplyContract = web3
     .eth
     .contract(supplyABI)
-    .at(supplyAddress);
+    .at(supplysupply);
 
 // get what we need, and then export it as a variable
 // TODO: probably best to load this into index.js and then add to global application state through redux
