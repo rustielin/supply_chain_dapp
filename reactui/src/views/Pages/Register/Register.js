dce4f8079fbf8311b7903d65a99b574aac4d2e0f
rustielin <rustielin@berkeley.edu>
2017-10-27 17:39:08 -0700

    add item screen for register

--- reactui/src/views/Pages/Register/Register.js
+++ reactui/src/views/Pages/Register/Register.js
@@ -10,36 +10,16 @@ class Register extends Component {
             <Col md="6">
               <Card className="mx-4">
                 <CardBlock className="card-body p-4">
-                  <h1>Register</h1>
-                  <p className="text-muted">Create your account</p>
+                  <h1>Add Item</h1>
+                  <p className="text-muted">Put your item on the marketplace</p>
                   <InputGroup className="mb-3">
-                    <InputGroupAddon><i className="icon-user"></i></InputGroupAddon>
-                    <Input type="text" placeholder="Username"/>
+                    <Input type="text" placeholder="Item name"/>
                   </InputGroup>
                   <InputGroup className="mb-3">
-                    <InputGroupAddon>@</InputGroupAddon>
-                    <Input type="text" placeholder="Email"/>
-                  </InputGroup>
-                  <InputGroup className="mb-3">
-                    <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
-                    <Input type="password" placeholder="Password"/>
-                  </InputGroup>
-                  <InputGroup className="mb-4">
-                    <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
-                    <Input type="password" placeholder="Repeat password"/>
+                    <Input type="text" placeholder="Price (eth)"/>
                   </InputGroup>
                   <Button color="success" block>Create Account</Button>
                 </CardBlock>
-                <CardFooter className="p-4">
-                  <Row>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-facebook" block><span>facebook</span></Button>
-                    </Col>
-                    <Col xs="12" sm="6">
-                      <Button className="btn-twitter" block><span>twitter</span></Button>
-                    </Col>
-                  </Row>
-                </CardFooter>
               </Card>
             </Col>
           </Row>
