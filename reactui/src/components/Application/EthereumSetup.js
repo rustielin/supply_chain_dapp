c70746a0b1d65300385292eddd15a07e416b101b
Derrick Li <derrickli@berkeley.edu>
2017-10-29 10:25:45 -0700

    connected frontend to rinkeby contract

--- reactui/src/components/Application/EthereumSetup.js
+++ reactui/src/components/Application/EthereumSetup.js
@@ -1,13 +1,13 @@
 import Web3 from 'web3';
-const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/'));
 
 
 // application binary interface
-import supplyABI from './supply_abi'
+//import supplyABI from './supply_abi' <- this one doesn't work I think
 
 // contract supply address
-var supplyAddress = '0xFd6603F30630C5aD67afBA6126b5c812682CEd54';
-
+var supplyAddress = '0x89e6f3c1c644393070aeb7fd3d7ed736a897201eab4c7c3cf4f509315cca2f74';
+var supplyABI = [{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"getItemOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"amount","type":"uint256"}],"name":"withdraw","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"item_id","type":"uint256"},{"name":"newPrice","type":"uint256"}],"name":"setItemPrice","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"getItemPrice","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"getFunds","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"getItemHistory","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"}],"name":"join","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"address"}],"name":"getOwnerBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"addFunds","outputs":[{"name":"","type":"string"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"getItemForSale","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"serial","type":"uint256"},{"name":"n","type":"string"}],"name":"addItem","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"address"}],"name":"getOwnerName","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"item_id","type":"uint256"}],"name":"changeToNotSellable","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"getItemName","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"itemID","type":"uint256"}],"name":"purchase","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"item_id","type":"uint256"}],"name":"changeToSellable","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"address"}],"name":"getOwnerItems","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"}];
 // get the reference
 const supplyContract = web3.eth.contract(supplyABI).at(supplyAddress);
 
@@ -22,6 +22,6 @@ var disp = {
     "contract": supplyContract,
 }
 
-alert(disp)
+alert(disp);
 
 export {supplyContract, disp};
