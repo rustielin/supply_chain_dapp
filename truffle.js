5e200ae6fe9a3672aac646a3c6179ab69bdc801c
garyli2198 <garyli2198@gmail.com>
2017-10-29 01:55:53 -0700

    added build

--- truffle.js
+++ truffle.js
@@ -1,9 +1,27 @@
+const HDWalletProvider = require("truffle-hdwallet-provider")
+var mnemonic = "summer onion delay final bitter artwork lens soon glide prevent word mail"
+
 module.exports = {
   networks: {
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/"),
+      network_id: "4",
+      gas: 4500000,
+      gasPrice: 25000000000
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io"),
+      network_id: "3",
+      gas: 4500000,
+      gasPrice: 25000000000
+    },
+
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     },
+
+
   }
 };
